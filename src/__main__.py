8f9f2ff62d423a482b316686ebbe2a88faddbe3f
Mac Button <demonzero14@gmail.com>
2021-09-01 11:26:42 -0700

    Change hero command to show growth ranking. Add tier lists for growth and max power

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,4 @@
-import time, threading, signal, sys, pytz, asyncio, os, argparse, sched, csv
+import time, threading, signal, sys, pytz, asyncio, os, argparse, sched, csv, difflib
 from pathlib import Path
 from pytz import timezone
 from datetime import datetime, timezone
@@ -16,8 +16,17 @@ client = discord.Client()
 BOSS_SCHEDULE = '!boss_schedule'
 EVENT_SCHEDULE = '!event_schedule'
 HERO = '!hero'
+POWER_TIER_LIST = '!power_tier_list'
+MILITARY_TIER_LIST = '!military_tier_list'
+FORTUNE_TIER_LIST = '!fortune_tier_list'
+PROVISIONS_TIER_LIST = '!provisions_tier_list'
+INSPIRATION_TIER_LIST = '!inspiration_tier_list'
 
 ##############################################################################
+# Global 1 line functions
+
+# Converts numbers to their orindal (1st, second etc)
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
 
 # Global variables
 BOSS_SCHEDULE_LIST = [
@@ -39,24 +48,63 @@ THURSDAY = 4
 FRIDAY = 5
 SATURDAY = 6
 
+HERO_NAME = 'Hero Name'
+MAX_POWER = 'Max Power'
+MILITARY_GROWTH = 'Military Growth'
+FORTUNE_GROWTH = 'Fortune Growth'
+PROVISIONS_GROWTH = 'Provisions Growth'
+INSPIRATION_GROWTH = 'Inspiration Growth'
+MAIDEN_GROWTH = 'Maiden Bond %'
+DIFFICULTY = 'Difficulty'
+
 hero_attributes_dict = []
 # CSV Import
 with open(Path(__file__).parent / '../resources/hero_attr_stats.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         hero_attributes_dict.append(row)
+
+def get_sorted_growths(type, difficulty=5):
+    growths = []
+    for dic in hero_attributes_dict:
+        if float(dic[DIFFICULTY]) > difficulty:
+            continue
+        growth = {}
+        growth[HERO_NAME] = dic[HERO_NAME]
+
+        if dic[type] == '':
+            dic[type] = 0
+        if dic[MAIDEN_GROWTH] == '':
+            dic[MAIDEN_GROWTH] = 0
+
+        growth['Growth'] = float(dic[type]) + float(dic[MAIDEN_GROWTH])
+        growths.append(growth)
+    return sorted(growths, key=lambda k: k['Growth'], reverse=True)
     
 def hero_growth_rank(hero_name, type):
-    # TODO: This
-    for i, dic in enumerate(hero_attributes_dict):
-        if dic['Hero Name'].lower() == hero_name:
-            return i
+    growths = get_sorted_growths(type)
+    for i, dic in enumerate(growths):
+        if dic[HERO_NAME].lower() == hero_name.lower():
+            return i + 1, dic['Growth']
+
+def hero_rank(hero_name, type):
+    sorted_heroes = sorted(hero_attributes_dict, key=lambda k: int(k[type]), reverse=True)
+    for i, dic in enumerate(sorted_heroes):
+        if dic[HERO_NAME].lower() == hero_name.lower():
+            return i + 1
 
 def hero_row(hero_name):
-    for i, dic in enumerate(hero_attributes_dict):
-        if dic['Hero Name'].lower() == hero_name:
+    for dic in hero_attributes_dict:
+        if dic[HERO_NAME].lower() == hero_name:
             return dic
     return None
+
+def hero_name_diff(command_hero_name):
+    heroes = []
+    for dic in hero_attributes_dict:
+        hero_name = dic[HERO_NAME].lower()
+        heroes.append(hero_name)
+    return difflib.get_close_matches(command_hero_name.lower(), heroes, cutoff=0.75)
 
 @client.event
 async def on_ready():
@@ -76,7 +124,7 @@ async def on_message(message):
     if command.startswith(EVENT_SCHEDULE):
         await message.channel.send(file=discord.File(Path(__file__).parent / '../resources/event_schedule.png'))
     if command.startswith(HERO):
-        parser = argparse.ArgumentParser(prog="!hero", add_help=False)
+        parser = argparse.ArgumentParser(prog=HERO, add_help=False)
         parser.add_argument('--detailed', '-d', action='store_true')
         parser.add_argument('hero')
         args = parser.parse_args(command.split()[1:])
@@ -86,14 +134,84 @@ async def on_message(message):
             if args.detailed:
                 print(entry)
             else:
-                await message.channel.send("{0}\nKingdom Power: {1} | Max Power: {2} | Military: {3} | Fortune: {4} | Provisions: {5} | Inspiration: {6}".format(entry['Hero Name'], entry['Max Power'], entry['Max KP'], entry['Max Military'], entry['Max Fortune'], entry['Max Provisions'], entry['Max Inspiration']))
+                ranks = [ordinal(hero_growth_rank(args.hero.lower(), MILITARY_GROWTH)[0]), ordinal(hero_growth_rank(args.hero.lower(), FORTUNE_GROWTH)[0]), ordinal(hero_growth_rank(args.hero.lower(), PROVISIONS_GROWTH)[0]), ordinal(hero_growth_rank(args.hero.lower(), INSPIRATION_GROWTH)[0])]
+                power_rank = ordinal(hero_rank(args.hero.lower(), MAX_POWER))
+                await message.channel.send("**{0}**\n Max Power Rating: {1} | Military KP Rank: {2} | Fortune KP Rank: {3} | Provisions KP Rank: {4} | Inspiration KP Rank: {5} | Difficulty {6}".format(entry[HERO_NAME], power_rank, ranks[0], ranks[1], ranks[2], ranks[3], entry[DIFFICULTY]))
+        else:
+            diffs = hero_name_diff(args.hero.lower())
+            await message.channel.send("Hero " + args.hero + " not found. Close hero names: " + str(diffs))
+    if command.startswith(POWER_TIER_LIST):
+        parser = argparse.ArgumentParser(prog=POWER_TIER_LIST, add_help=False)
+        parser.add_argument('--new', '-n', action='store_true')
+        parser.add_argument('--low_econ', '-l', action='store_true')
+        args = parser.parse_args(command.split()[1:])
+
+        if args.new and args.low_econ:
+            await message.channel.send("Can only specify one of new or low econ")
+
+        difficulty = 5
+        if args.new:
+            difficulty = 2
+        if args.low_econ:
+            difficulty = 4
+        
+        tier_list = sorted(hero_attributes_dict, key=lambda k: int(k[MAX_POWER]), reverse=True)
+        tier_list = list(filter(lambda k: float(k[DIFFICULTY]) <= difficulty, tier_list))
+        tier_list_str = "**Power Tier List**\n"
+        rank = 1;
+        for hero in tier_list[:20]:
+            tier_list_str += str(rank) + ". " + hero['Hero Name'] + " (" + hero[MAX_POWER] + ")\n"
+            rank += 1
+        await message.channel.send(tier_list_str)
+    if command.startswith(MILITARY_TIER_LIST):
+        tier_list = create_growth_tier_list(MILITARY_GROWTH, 5, 20)
+        tier_list_str = "**Military Tier List**\n"
+        rank = 1;
+        for hero in tier_list:
+            tier_list_str += str(rank) + ". " + hero['Hero Name'] + " (" + str(round(hero['Growth'] * 100)) + "%)\n"
+            rank += 1
+        await message.channel.send(tier_list_str)
+    if command.startswith(FORTUNE_TIER_LIST):
+        tier_list = create_growth_tier_list(FORTUNE_GROWTH, 5, 20)
+        tier_list_str = "**Fortune Tier List**\n"
+        rank = 1;
+        for hero in tier_list:
+            tier_list_str += str(rank) + ". " + hero['Hero Name'] + " (" + str(round(hero['Growth'] * 100)) + "%)\n"
+            rank += 1
+        await message.channel.send(tier_list_str)
+    if command.startswith(PROVISIONS_TIER_LIST):
+        tier_list = create_growth_tier_list(PROVISIONS_GROWTH, 5, 20)
+        tier_list_str = "**Provisions Tier List**\n"
+        rank = 1;
+        for hero in tier_list:
+            tier_list_str += str(rank) + ". " + hero['Hero Name'] + " (" + str(round(hero['Growth'] * 100)) + "%)\n"
+            rank += 1
+        await message.channel.send(tier_list_str)
+    if command.startswith(INSPIRATION_TIER_LIST):
+        tier_list = create_growth_tier_list(INSPIRATION_GROWTH, 5, 20)
+        tier_list_str = "**Inspiration Tier List**\n"
+        rank = 1;
+        for hero in tier_list:
+            tier_list_str += str(rank) + ". " + hero['Hero Name'] + " (" + str(round(hero['Growth'] * 100)) + "%)\n"
+            rank += 1
+        await message.channel.send(tier_list_str)
 
 
-    
+def create_growth_tier_list(type, difficulty, cutoff):
+    growths = get_sorted_growths(type, difficulty)
+    return list(growths)[:cutoff]
 
 async def help(message):
-    await message.channel.send(BOSS_SCHEDULE + ' :Prints the current member list that is allowed to kill bosses')
-    await message.channel.send(EVENT_SCHEDULE + ' :Posts an image of the event schedule for challenges and cross server events')
+    await message.channel.send(BOSS_SCHEDULE + ': Prints the current member list that is allowed to kill bosses')
+    await message.channel.send(EVENT_SCHEDULE + ': Posts an image of the event schedule for challenges and cross server events')
+    await message.channel.send(HERO + ': Shows the rating of the hero compared to others. Use -d to see fully detailed stats')
+    await message.channel.send('---------------------------------------------------------------------------')
+    await message.channel.send('All tier lists can use the "--low_econ" or "new" flags to create a tier list geared towards lower spenders or new players')
+    await message.channel.send(POWER_TIER_LIST + ': Tier list for the strongest hero\'s rated by maximum power')
+    await message.channel.send(MILITARY_TIER_LIST + ': Tier list for military growth')
+    await message.channel.send(FORTUNE_TIER_LIST + ': Tier list for fortune growth')
+    await message.channel.send(PROVISIONS_TIER_LIST + ': Tier list for provisions growth')
+    await message.channel.send(INSPIRATION_TIER_LIST + ': Tier list for inspiration growth')
     
 def boss_schedule():
     boss_schedule = BOSS_SCHEDULE_LIST.copy()
