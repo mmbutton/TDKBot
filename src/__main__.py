6304ec20a225441c158e649f8fdba6a434d8a35b
Mac Button <demonzero14@gmail.com>
2022-03-21 18:21:37 -0700

    Add new commands to help

--- src/__main__.py
+++ src/__main__.py
@@ -454,6 +454,9 @@ async def help(message):
     # All server commands
     await message.channel.send(EVENT_SCHEDULE + ': Posts an image of the event schedule for challenges and cross server events')
     await message.channel.send(HERO + ': Shows the rating of the hero compared to others. Use -d to see fully detailed stats, use -i to pull up an infographic.')
+    await message.channel.send(FORMULAS + ': Pulls up a formula sheet with a bunch of useful formulas such as KP, power etc.')
+    await message.channel.send(ZODIACS + ': Pulls up a screenshot showing Zodiacs, their maidens and their paragons.')
+    await message.channel.send(CASTLE_SKINS + ': Pulls up a screenshot of all current castle skins and there effects.')
     await message.channel.send('---------------------------------------------------------------------------')
     await message.channel.send('All tier lists can use the low VIP "-l" or new player "-n" flags to create a tier list geared towards lower spenders or new players')
     await message.channel.send(KP_TIER_LIST + ': Tier list for heroes maximum KP')
@@ -564,7 +567,7 @@ def notifier_thread():
     schedule.every().wednesday.at(reset_hour_str).do(wednesday_notifier)
     schedule.every().friday.at(reset_hour_str).do(friday_notifier)
 
-    boss_free_for_all = (reset_hour - 4) % 24
+    boss_free_for_all = (reset_hour - 2) % 24
     boss_free_for_all_str = '{:02d}:30'.format(boss_free_for_all)
     schedule.every().day.at(boss_free_for_all_str).do(boss_free_for_all_notifier)
 
